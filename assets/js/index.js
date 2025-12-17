09026b7c77a614383200a3c7f648efc7eca32cd6
simoninvoker <simonalhalabi@gmail.com>
2025-12-17 03:51:39 +0100

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,7 +8,7 @@ VANTA.NET({
 });
 
 const supabaseUrl = 'https://qxpaplabjocxaftqocgu.supabase.co';
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF4cGFwbGFiam9jeGFmdHFvY2d1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU2NTcyOTQsImV4cCI6MjA4MTIzMzI5NH0.VpoV9d2XGkRTv5UoZFKiA23IOOV2zasV18pW_9JmCj4";
+const supabaseAnonKey = "sb_publishable_he7X4Xjj74CcZfRue2RVTg_UsJSbyYC";
 const { createClient } = window.supabase;
 const supabaseClient = createClient(supabaseUrl, supabaseAnonKey);
 
@@ -198,4 +198,5 @@ supabaseClient.auth.onAuthStateChange((event, session) => {
 });
 
 // Initial UI update on page load
+
 updateUI();
